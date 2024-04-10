08436874de8a25264bbc69ae68c8f00fb6a57eb3
Omprakash Choudhary <omprakash choudhary>
2024-04-10 05:56:21 +0530

    access to landing page

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -1,23 +1,34 @@
+import { useAuthContext } from "@/context/authcontext";
 import Image from "next/image";
 import Link from "next/link";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
 
 export default function Home() {
+  const { auth } = useAuthContext();
+  const router = useRouter();
+
   return (
     <div className="flex justify-center items-center h-screen">
-      <div className="flex gap-3 items-center">
-        <Link href="/login">
-          {" "}
-          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-            Login{" "}
-          </button>
-        </Link>
+      {auth === false && (
+        <div className="flex gap-3 items-center">
+          <Link href="/login">
+            {" "}
+            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+              Login{" "}
+            </button>
+          </Link>
 
-        <Link href="/signup">
-          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-            Signup{" "}
-          </button>
-        </Link>
-      </div>
+          <Link href="/signup">
+            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+              Signup{" "}
+            </button>
+          </Link>
+        </div>
+      )}
+      {auth === true && (
+        <div className="flex flex-col items-center gap-5">LANDING PAGE</div>
+      )}
     </div>
   );
 }
