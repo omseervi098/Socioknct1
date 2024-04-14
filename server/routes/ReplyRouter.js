c840fcc3885946d8b099387a4ab3a8242244b7e6
Omprakash Choudhary <omprakash choudhary>
2024-04-14 23:51:15 +0530

    added modals for videoupload, document uploads, image uploads with their apis

--- server/routes/ReplyRouter.js
+++ server/routes/ReplyRouter.js
@@ -2,7 +2,7 @@ import { Router } from "express";
 const router = Router();
 router.get("/", (req, res) => {
   res.send({
-    message: "Welcome to Socioknct API",
+    message: "Welcome to Reply API",
   });
 });
 export default router;
