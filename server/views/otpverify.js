fa2d5841225e9669fb30c3232b0354adbe189040
John Doe <omseervi098@gmail.com>
2024-04-27 19:34:17 +0530

    added logo

--- server/views/otpverify.js
+++ server/views/otpverify.js
@@ -14,7 +14,7 @@ export const verifyemailhtml = async (user, otp) => {
     <div  style="background-color:#efefef;gap:10px;font-family:'poppins';">
       <div  style="background-color:#1B8DFF;padding: 10px;">
         <div style="color:white;font-size: 1.5rem;display:flex;align-items:center;">
-          Socioknct <image src="https://socioknct.vercel.app/logo2_bg.png" width="30" height="30" alt="logo">
+          Socioknct <image src="https://socioknct.vercel.app/logo2_bg.png"  alt="logo" style="margin-left:10px;width:30px;height:30px;"/>
         </div>
       </div>
       <div style="font-size: 0.9rem;padding:10px;">
