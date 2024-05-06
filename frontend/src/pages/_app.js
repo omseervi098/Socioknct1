b432a4c55d68de4f42cb1cec015512556ff68b69
John Doe <omseervi098@gmail.com>
2024-05-06 15:45:30 +0530

    fixing loader

--- frontend/src/pages/_app.js
+++ frontend/src/pages/_app.js
@@ -26,7 +26,7 @@ export default function App({ Component, pageProps }) {
       import.meta.url
     ).toString();
   }, []);
-  const [loading, setLoading] = React.useState(false);
+  const [loading, setLoading] = React.useState(true);
   useEffect(() => {
     setLoading(true);
     setTimeout(() => {
